b25321e31f4b8bfbc02b14b5606c5cc4702bd5e7
Artur Junior <arturcmjr@gmail.com>
2022-04-14 08:54:19 -0300

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { SharedModule } from './shared/shared.module';
 import { ModulesModule } from './modules/modules.module';
 import { AuthGuard } from '@shared/guards/auth/auth.guard';
 import { LoginGuard } from '@shared/guards/login/login.guard';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, SharedModule, ModulesModule],
+  imports: [AppRoutingModule, SharedModule, ModulesModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [AuthGuard, LoginGuard],
   bootstrap: [AppComponent],
 })
